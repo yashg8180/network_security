1443b7968a6099319057a0021248b0eece7fac9e
yash <yashgcode@gmial.com>
2025-04-05 10:46:58 +0530

    logging and exception

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,24 @@
+import sys
+from networksecurity.logging import logger  # ✅ Make sure logger is correctly imported
+
+class NetworkSecurityException(Exception):
+    def __init__(self, error_message, error_details: sys):
+        self.error_message = error_message
+        _, _, exc_tb = error_details.exc_info()
+
+        self.lineno = exc_tb.tb_lineno
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
+
+    def __str__(self):
+        return "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+            self.file_name, self.lineno, str(self.error_message)
+        )
+
+# if __name__ == '__main__':
+#     try:
+#         logger.info("ENTER THE TRY BLOCK")  
+#         a = 1 / 0
+#         print("This will not be printed", a)
+#     except Exception as e:
+#         logger.error(f"Exception occurred: {e}")  
+#         raise NetworkSecurityException(e, sys)
